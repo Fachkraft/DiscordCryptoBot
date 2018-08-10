979f931d2becd20c7cfeb943c6e528885b104663
Fachkraft <42253656+Fachkraft@users.noreply.github.com>
2018-08-10 14:15:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,27 @@
 import discord
 from discord.ext import commands
 
+const version = '0.0.1'
 TOKEN = 'CryptoBot_Token'
 
 description = '''ninjaBot in Python'''
-bot = commands.Bot(command_prefix='?', description=description)
+bot = commands.Bot(command_prefix='/', description=description)
 
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
-    print('------')
+    await bot.say("Type !commands")
 
 
 @bot.command()
-async def hello():
-    """Says world"""
-    await bot.say("world")
+async def version():
+    await bot.say(version)
 
 
 @bot.command()
-async def add(left : int, right : int):
-    """Adds two numbers together."""
-    await bot.say(left + right)
+async def commands
+    await bot.say("!version, !commands")
 
 bot.run(TOKEN)
