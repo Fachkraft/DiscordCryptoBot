ae8fa9514eeee6596a817978ce80f0334acffda5
Fachkraft <42253656+Fachkraft@users.noreply.github.com>
2018-08-10 19:22:05 +0200

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -3,4 +3,8 @@ import requests
 link = 'https://api.cryptowat.ch/markets/kraken/btceur/price'
 fullText = requests.get(link)
 decryptedText = fullText.text
+decryptedText = decryptedText.replace('{','').replace('"','').replace('result','').replace(':','').replace('price','').replace('allowance','').replace(',','').replace('cost','').replace('}','').replace('remaining','')
+decryptedText = decryptedText[:-15]
+decryptedText = decryptedText + ' Euro'
+
 
